c654e5b0083b62517c1a91e3886f61518d711bd8
SvenZerkov <54982640+SvenZerkov@users.noreply.github.com>
2021-11-22 15:27:51 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Talo talo = new Talo("omakotitalo", 1962, 130, 3, "Litukkaperä 4", 5);
+		
+		System.out.println(talo);
+	}
+
+}
---
+++ src/Talo.java
@@ -0,0 +1,109 @@
+
+public class Talo {
+	private String tyyli;
+	private int rakennusVuosi;
+	private double pintaAla;
+	private int kerrosLkm;
+	private String osoite;
+	private int huoneLkm;
+	
+	private boolean onkoAsuttu;
+	private boolean onkoLammin;
+	private boolean tarvitseekoRemonttia;
+	
+	public Talo() {
+		
+	}
+	
+	public Talo(String tyyli, int rakennusvuosi, double pintaAla, int kerrosLkm, String osoite, int huoneLkm) {
+		this.tyyli = tyyli;
+		this.rakennusVuosi = rakennusvuosi;
+		this.pintaAla = pintaAla;
+		this.kerrosLkm = kerrosLkm;
+		this.osoite = osoite;
+		this.huoneLkm = huoneLkm;
+		this.onkoAsuttu = true;
+		this.onkoLammin = true;
+		this.tarvitseekoRemonttia = false;
+	}
+	
+	public boolean onkoAsuttu() {
+		return this.onkoAsuttu;
+	}
+	
+	public boolean onkoLammin() {
+		return this.onkoLammin;
+	}
+	
+	public boolean tarvitseekoRemonttia() {
+		return this.tarvitseekoRemonttia;
+	}
+	
+	public void setRemontti(String vaite) {
+		if (vaite.equals("Kyllä")) {
+			this.tarvitseekoRemonttia = true;
+		} else {
+			this.tarvitseekoRemonttia = false;
+		}
+	}
+	
+	
+
+	@Override
+	public String toString() {
+		return "Talo [tyyli=" + tyyli + ", rakennusVuosi=" + rakennusVuosi + ", pintaAla=" + pintaAla + ", kerrosLkm="
+				+ kerrosLkm + ", osoite=" + osoite + ", huoneLkm=" + huoneLkm + "]";
+	}
+
+	public String getTyyli() {
+		return tyyli;
+	}
+
+	public void setTyyli(String tyyli) {
+		this.tyyli = tyyli;
+	}
+
+	public int getRakennusVuosi() {
+		return rakennusVuosi;
+	}
+
+	public void setRakennusVuosi(int rakennusVuosi) {
+		this.rakennusVuosi = rakennusVuosi;
+	}
+
+	public double getPintaAla() {
+		return pintaAla;
+	}
+
+	public void setPintaAla(double pintaAla) {
+		this.pintaAla = pintaAla;
+	}
+
+	public int getKerrosLkm() {
+		return kerrosLkm;
+	}
+
+	public void setKerrosLkm(int kerrosLkm) {
+		this.kerrosLkm = kerrosLkm;
+	}
+
+	public String getOsoite() {
+		return osoite;
+	}
+
+	public void setOsoite(String osoite) {
+		this.osoite = osoite;
+	}
+
+	public int getHuoneLkm() {
+		return huoneLkm;
+	}
+
+	public void setHuoneLkm(int huoneLkm) {
+		this.huoneLkm = huoneLkm;
+	}
+	
+	
+	
+	
+}
